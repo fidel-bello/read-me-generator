7ce454a53e4e92fa31c66b2ecaf4ebb3e9342e42
fidel bello <59423@bears.smcc.edu>
2021-02-12 16:57:53 -0500

    finished questions array and markDown file

--- index.js
+++ index.js
@@ -9,18 +9,62 @@ const questions = [
     message: "what is the title for your project?",
     name: "Title",
 },
-{
+{ 
     type: "input",
-    message: "PLease give description of your project",
-    name: "description",
+    message: "What is your Github username?",
+    name: "UserName"
+
 },
 {
     type: "input",
-    message: "What are neccessary dependancies for this application",
+    message: "PLease give description of your project.",
+    name: "Description",
+},
+{
+    type: "input",
+    message: "What are neccessary dependancies for this application?",
     name: "Installation",
 },
+{
+    type: "input", 
+    message: "what is this project used for?",
+    name: "Usage",
+},
+{
+    type: "list", 
+    message: "choose license for this project: ",
+    name: "License",
+    choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open",
+    ]
+},
+{
+    type: "input",
+    message: "Who all is contributing to this project?",
+    name: "Contributing",
+},
+{
+    type: "input",
+    message: "how do you test this project?",
+    name: "Test",
+},
+{
+    type: "input",
+    message: "What to do if users have questions?",
+    name: "Questions",
+},
+{
+    type: "input",
+    message: "What is your email?",
+    name: "Email",
+},
 ];
-console.log(questions)
 // function to write README file
 function writeToFile(fileName, data) {
 }
--- utils/markDown.js
+++ utils/markDown.js
@@ -1,9 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `
+    <h1 align="center">${data.Title}</h1>
     [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/${data.UserName}/${data.Title})
-    # Description
+   
+    ## Description
+    * ${data.Description}
+
+    ## Table of Contents
+   -[Description](#Description)
+   -[Installation](#Installation)
+   -[Usage](#Usage)
+   -[licence](#License)
+   -[Contributing](#Contributing)
+   -[Test](#Test)
+   -[Questions](#Questions)
+   
+ 
+
+   ## Installation
+   * ${data.Installation} 
+
+   ## Usage 
+   * ${data.Usage}
+
+   ## License
+   * ![badge](https://img.shields.io/badge/license-${data.License}-brightgreen)
+   <br />
+   * This application is covered by ${data.License}
+
+   ## Contributing
+   * ${data.Contributing}
+
+   ## Test
+   * ${data.Test}
+
+   ## Questions
+   * ${data.Questions}
+   * :octocat: My github is: [${data.UserName}](https://github.com/${data.UserName})<br />
+   *  Email me with questions: ${data.Email}<br /><br />
+
   `;
   }
-  
   module.exports = generateMarkdown;
